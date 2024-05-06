527e9012b517562419e1dcb64e5e068d4e235de6
MographlloCode <gmellocontact@gmail.com>
2024-05-06 01:28:39 -0300

    fixed projects grid

--- app/_components/parts/ProjectPage/ProjectCards.jsx
+++ app/_components/parts/ProjectPage/ProjectCards.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 const ProjectCards = ({project_infos}) => {
   return (
     project_infos.card_grid.map((column, i) => (
-        <div key={i} className='grid grid-cols-3 grid-rows-12 w-full gap-4 md:gap-8'>
+        <div key={i} className='grid grid-cols-3 grid-rows-13 w-full gap-4 md:gap-8'>
           {column.cards.map((card, i) => (
-            <div key={i} className={`col-start-1 col-end-4 row-start-${card.row_start} relative row-end-${card.row_end} border border-[#00171F] dark:border-[#F4E285] transition-all ease-in-out group`}>
+            <div key={i} className={`relative col-start-1 col-end-4 row-start-${card.row_start} row-end-${card.row_end} border border-[#00171F] dark:border-[#F4E285] transition-all ease-in-out group`}>
               <div className='absolute inset-0 z-0'>
                 <Image src={card.card_image} alt='tripo' layout='fill' objectFit='cover' />
               </div>
--- app/_components/parts/ProjectPage/ProjectHeader.jsx
+++ app/_components/parts/ProjectPage/ProjectHeader.jsx
@@ -7,7 +7,7 @@ const ProjectHeader = ({project_infos}) => {
         <nav className='flex max-w-80 md:max-w-[36rem] justify-end md:justify-start'>
             <ReturnButton/>
         </nav>
-        <section className='flex flex-col gap-4 w-fit text-justify max-w-80 md:max-w-[36rem] md:text-start'>
+        <section className='flex flex-col gap-4 w-fit text-end max-w-80 md:max-w-[36rem] md:text-justify'>
             <h1 className='kenoky-light text-3xl md:text-text-5xl lg:text-7xl xl:text-9xl uppercase text-[#00171F] dark:text-[#F4E285] '>{project_infos.project_title}</h1>
             <p className='montserrat text-sm md:text-text-md lg:text-xl xl:text-2xl dark:text-[#F8F7FF] text-[#00171F]'>{project_infos.project_description}</p>
         </section>
